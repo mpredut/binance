dea31d5a24f4752f5c3dc61d3c0bfd9036ae654c
marius predut <marius@dotlumen.com>
2024-08-07 18:50:16 +0300

    update ....

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ order_cost_btc = 0.00004405  # BTC
 price_change_threshold = 0.007  # Pragul de schimbare a prețului, 0.7%
 #price_change_threshold = 0.07  # Pragul de schimbare a prețului, 7%
 interval_time = 2 * 3600 # 2 h * 3600 seconds.
-interval_time = 1 * 79
+interval_time = 97 * 79
 
 def get_interval_time(valoare_prestabilita=interval_time, marja_aleatoare=100):
     # Generarea unei valori aleatoare în intervalul [-marja_aleatoare, marja_aleatoare]
