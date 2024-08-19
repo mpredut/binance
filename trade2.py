88759cdc90f96c01c6c7f1b84c96dd1bee227658
marius predut <marius@dotlumen.com>
2024-08-19 23:30:14 +0300

    FIX print

--- trade2.py
+++ trade2.py
@@ -118,7 +118,7 @@ class PriceWindow:
             return 0
 
         slope = (max_price - min_price) / (max_index - min_index)
-        print(f"Slope calculated: {slope}")
+        print(f"Slope calculated: {slope:.f2}")
 
         return slope
 
