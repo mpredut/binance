81abe969f9dae06b6358dbc28bb3a12bbfecda1e
marius predut <marius@dotlumen.com>
2024-09-12 21:02:54 +0300

    fix

--- trade2.py
+++ trade2.py
@@ -389,9 +389,9 @@ while True:
                 print(f"End of UP trend. SELL order at {proposed_price:.2f} EUR")
                 #order_placed, order_id = track_and_place_order('SELL', proposed_price, current_price, slope=None, order_placed=order_placed, order_id=order_id)
             elif expired_trend == 'DOWN':
-                proposed_price = proposed_price - 142  # Preț de cumpărare
+                proposed_price = proposed_price - 242  # Preț de cumpărare
                 print(f"End of DOWN trend. BUY order at {proposed_price:.2f} EUR")
-                #order_placed, order_id = track_and_place_order('BUY', proposed_price, current_price, slope=None, order_placed=order_placed, order_id=order_id)
+                order_placed, order_id = track_and_place_order('BUY', proposed_price, current_price, slope=None, order_placed=order_placed, order_id=order_id)
             #last_order_time = current_time
 
         # Verificăm schimbările de preț și gestionăm trendurile
