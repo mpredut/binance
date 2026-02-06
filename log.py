3985a5e36d6f74b31167cd22723f7ca80834861f
marius predut <marius@dotlumen.com>
2026-02-06 11:43:51 +0200

    update log to get stderror also + file size management

--- log.py
+++ log.py
@@ -3,9 +3,72 @@ import builtins
 import os
 import sys
 import datetime
+import threading
 
-process_id = os.getpid()
+MAX_TOTAL_LOG_SIZE = 1000 * 1024 * 1024 # 1000 MB
+CHECK_EVERY_WRITES = 1000
 
+write_counter = 0
+
+def get_folder_size(folder):
+    total = 0
+    for f in os.listdir(folder):
+        path = os.path.join(folder, f)
+        if os.path.isfile(path):
+            total += os.path.getsize(path)
+    return total
+
+
+def delete_oldest_log(folder):
+    files = []
+    for f in os.listdir(folder):
+        path = os.path.join(folder, f)
+        if os.path.isfile(path):
+            files.append(path)
+
+    if not files:
+        return
+
+    oldest = min(files, key=os.path.getmtime)
+
+    try:
+        os.remove(oldest)
+        original_print(f"[LOGGER] deleted oldest log: {os.path.basename(oldest)}")
+    except Exception as e:
+        original_print(f"[LOGGER] failed to delete log: {e}")
+
+
+def enforce_logger_size(folder):
+    global write_counter
+    write_counter += 1
+    if write_counter < CHECK_EVERY_WRITES:
+        return
+
+    write_counter = 0
+
+    try:
+        total_size_bytes = get_folder_size(folder)
+        total_size_kb = total_size_bytes / 1024
+        max_size_kb = MAX_TOTAL_LOG_SIZE / 1024
+
+        original_print(
+            f"[LOGGER] Folder '{folder}' are {total_size_kb:.2f} KB, "
+            f"maxim permis {MAX_TOTAL_LOG_SIZE / 1024:.2f} KB"
+        )
+
+        while total_size_bytes >= MAX_TOTAL_LOG_SIZE:
+            original_print(
+                f"[LOGGER] WARNING: Folder '{folder}' depaseste marimea maxima de "
+                f"{max_size_kb:.2f} KB → voi sterge cel mai vechi fisier"
+            )
+            delete_oldest_log(folder)
+            total_size_bytes = get_folder_size(folder)
+
+    except Exception as e:
+        original_print(f"[LOGGER] cleanup error: {e}")
+
+
+# main code
 # Configurare de baza pentru logging
 #logging.basicConfig(
 #    level=logging.INFO,  # Seteaza nivelul de logging dorit
@@ -19,63 +82,85 @@ process_id = os.getpid()
 
 #logger = logging.getLogger(__name__)  # Obtine un logger
 
-# Salveaza functia originala print
-original_print = builtins.print
+process_id = os.getpid()
 
+original_print = builtins.print
 
 # Obtine numele aplicatiei Python
 app_name = os.path.splitext(os.path.basename(__file__))[0]
 app_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 
-
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-import threading
 lock = threading.Lock()
 
-PRINT_CONTEXT = None
-# Redefineste functia print pentru a adauga logare
+log_folder = "logger"
+PRINT_CONTEXT = threading.local()
+
+
 def print(*args, **kwargs):
-    if PRINT_CONTEXT is not None:
-        if not getattr(PRINT_CONTEXT, "enable_print", True):
-            original_print(f"BLOCK >>>>> BLOCK ...")
-            return  # blocăm afișarea
-    # Converteste toate argumentele în stringuri si le uneste într-un singur mesaj
+    if hasattr(PRINT_CONTEXT, "enable_print") and not PRINT_CONTEXT.enable_print:
+        original_print("BLOCK >>>>> BLOCK ...")
+        return
+
     message = " ".join(map(str, args))
-    
     new_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    
+
     with lock:
         global current_date
         if new_date != current_date:
             current_date = new_date
-        
-        bot_folder = "bot_logger"
-        if not os.path.exists(bot_folder):
-            os.makedirs(bot_folder)
-        
-        # Construieste calea completa a fisierului de log, incluzand numele aplicatiei, data curenta si ID-ul procesului
-        log_file_path = os.path.join(os.getcwd(), bot_folder, f"{app_name}_{current_date}_pid{process_id}.log")
-        
-        # Obtine ora si minutul curent
+
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+
+        log_file_path = os.path.join(
+            os.getcwd(), log_folder, f"{app_name}_{current_date}_pid{process_id}.log"
+        )
+
         current_time = datetime.datetime.now().strftime("%H:%M")
 
-        # Apeleaza functia print originala
         original_print(f"{current_time} {message}", **kwargs)
-        # Scrie mesajul în fisierul de log
+
         try:
             with open(log_file_path, "a") as log_file:
                 log_file.write(f"{current_time} {message}\n")
         except PermissionError as e:
             original_print(f"Error writing log: {e}")
 
+        enforce_logger_size(log_folder)
 
-
-# Redefineste functia print din builtins pentru a functiona în întreg codul
+# Redefineste functia
 builtins.print = print
 
 def dumy_print(*args, **kwargs):
     pass
-    
+
 def disable_print():
     builtins.print = dumy_print
+
+
+log_file_path = os.path.join(
+    os.getcwd(), log_folder, f"{app_name}_{current_date}_pid{process_id}.log"
+)
+
+
+# std err wrapper
+original_stderr_write = sys.stderr.write
+
+def stderr_write(message):
+    if not message.strip():
+        return
+
+    current_time = datetime.datetime.now().strftime("%H:%M")
+
+    try:
+        with open(log_file_path, "a") as log_file:
+            log_file.write(f"{current_time} {message}")
+    except Exception:
+        pass
+
+    original_stderr_write(message)
+
+
+sys.stderr.write = stderr_write
